d6c11b149885fcdfa2780b9620530f5d2c257f73
thayrov <thayrovg@gmail.com>
2024-01-02 12:37:32 -0600

    🐛 [FIX]: Deploy changes

--- app/Legal/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Legal() {
-  return <main className='flex flex-col items-center px-4'>Aviso Legal</main>;
-}
--- app/Privacidad/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Privacidad() {
-  return <main className='flex flex-col items-center px-4'>Aviso de Privacidad</main>;
-}
